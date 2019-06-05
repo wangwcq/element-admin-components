16a02a8f0add6415dfb4d645cf914b4aa79dd836
Chris Wang <chris.wang@mioying.com>
2019-06-05 14:26:54 +0800

    ER: Add log to file

--- logger.js
+++ logger.js
@@ -1,10 +1,28 @@
-const _ = require('lodash');
-const moment = require('moment');
 const pino = require('pino');
 
-module.exports = (name) => pino({
-  name,
-  prettyPrint: {
-    translateTime: 'SYS:standard',
-  },
-});
+let commonLoggerDest = null;
+
+module.exports = (name, loggerDest = null) => {
+  let pinoDestination = undefined;
+  if (loggerDest) {
+    pinoDestination = pino.destination(loggerDest);
+  } else if (commonLoggerDest) {
+    pinoDestination = pino.destination(commonLoggerDest);
+  }
+  return pino({
+    name,
+    prettyPrint: {
+      translateTime: 'SYS:standard',
+    },
+    ...(pinoDestination ? {
+      prettyPrint: false,
+    } : {}),
+  }, pinoDestination);
+};
+
+module.exports.init = (config = {}) => {
+  const {
+    dest = null,
+  } = config;
+  commonLoggerDest = dest;
+};
